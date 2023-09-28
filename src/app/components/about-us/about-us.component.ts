820caa43a4d99ec4dfb1d44befd4ae14b15e542d
Elmor Cabalfin <cabalfinelmor17@gmail.com>
2023-09-28 18:47:57 +0800

    Added i18n translations

--- src/app/components/about-us/about-us.component.ts
+++ src/app/components/about-us/about-us.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { TranslationService } from 'src/app/shared/services/translation-service';
 
 @Component({
   selector: 'app-about-us',
@@ -6,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./about-us.component.sass']
 })
 export class AboutUsComponent {
+  
+  constructor(
+    private route: ActivatedRoute,
+    private translationService: TranslationService,
+    private translate: TranslateService
+  ) {}
+
+  ngOnInit() {
+    this.translationService.init();
+
+    this.route.queryParams
+    .subscribe(params => {
+      if (params.hasOwnProperty("lang")) {
+        this.translationService.setLanguage(params["lang"]);
+      }
+    }
+  );    
+  }
+
+  changeLanguage(lang: string) {
+    this.translationService.setLanguage(lang);
+  }
 
 }
